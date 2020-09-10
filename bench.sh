5725abe45b8394530a6140c46f8143a31fa4c9d3
Nicolas Derumigny <nderumigny@gmail.com>
2020-09-10 18:34:35 +0200

    updating gitignore + resilience to file links

--- bench.sh
+++ bench.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
-DIR=`dirname $0`
+SELF=`readlink -f $0`
+DIR=`dirname $SELF`
 if [ "$1" == "" ]; then
 	NB_THREADS=$((`lscpu | grep "^CPU(s):" | sed -E "s/.* ([0-9]+)/\1/g"`))
 else
 	NB_THREADS=$1
 fi
-cd $DIR
 
+cd $DIR
 time 7z a -m0=lzma2 out.7z data/
+cd -
