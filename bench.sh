61a5028502249dba33c7e623b1980e3b6a12ca04
Nicolas Derumigny <nderumigny@gmail.com>
2024-10-06 09:44:20 +0200

    fix(bench): delete archive before re-compressing

--- bench.sh
+++ bench.sh
@@ -5,9 +5,12 @@ DIR=`dirname $SELF`
 if [ "$1" == "" ]; then
     NB_THREADS=$((`lscpu | grep "^Processeur(s)" | sed -E "s/.* ([0-9]+)/\1/g"`))
 else
-	NB_THREADS=$1
+    NB_THREADS=$1
 fi
 
 cd $DIR
+if [ -f out.7z ]; then
+    rm out.7z
+fi
 /bin/time -p 7z a -m0=lzma2 out.7z data/
 cd -
